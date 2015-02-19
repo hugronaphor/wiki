651915ad2f80d41e14a26f391b711d5a68fadd10
Cornel Andreev <hugronaphor@gmail.com>
2015-02-19 00:01:04 +0000

    Prepare for new design.

---
+++ sites/all/themes/cornel/js/main.js
@@ -0,0 +1,23 @@
+(function($) {
+
+  'use strict';
+
+  /**
+   * A simple Drupal behavior example.
+   */
+  Drupal.behaviors.cirlceThemeExample = {
+    attach: function(context) {
+      
+    }
+  };
+
+  Drupal.behaviors.generalItemHover = {
+    attach: function(context) {
+
+      // Animate text on hover for Items.
+      // TO DO.
+
+    }
+  };
+
+})(jQuery);
